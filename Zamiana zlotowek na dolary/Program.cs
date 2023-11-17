3fcebedb225ecfcae670f66e2845bbe26aa62ca5
biQte <patrykgonet@gmail.com>
2023-11-17 12:31:44 +0100

    Add project files.

---
+++ Zamiana zlotowek na dolary/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Zamiana_zlotowek_na_dolary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zamiana złotówek na dolary");
+            Console.WriteLine("Podaj kwotę w złotówkach:");
+            float pln = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podana kwota zamieniona na dolary wynosi: $" + Math.Round((pln * 4.03), 2));
+        }
+    }
+}
